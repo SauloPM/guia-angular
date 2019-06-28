c01a4bea217e187f6fb85aa290e0837437d77c7c
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-06-28 10:00:51 +0100

    [ Componente ngSwitch ] » Componente creado y maquetado

--- src/app/componentes/directivas/ngswitch/ngswitch.component.ts
+++ src/app/componentes/directivas/ngswitch/ngswitch.component.ts
@@ -1,14 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ngswitch',
   templateUrl: './ngswitch.component.html'
 })
-export class NgswitchComponent implements OnInit {
+export class NgswitchComponent {
+
+  // Atributos
+  tipoAlerta: string = 'info';
 
   constructor() { }
 
-  ngOnInit() {
+  cambiarAlerta( tipo: string ) {
+    switch ( tipo ) {
+      case '1' : {
+        this.tipoAlerta = 'info';
+        break;
+      }
+      case '2' : {
+        this.tipoAlerta = 'primary';
+        break;
+      }
+      case '3' : {
+        this.tipoAlerta = 'secondary';
+        break;
+      }
+      case '4' : {
+        this.tipoAlerta = 'success';
+        break;
+      }
+      case '5' : {
+        this.tipoAlerta = 'danger';
+        break;
+      }
+      default : {
+        this.tipoAlerta = 'warning';
+        break;
+      }
+    }
   }
 
 }
