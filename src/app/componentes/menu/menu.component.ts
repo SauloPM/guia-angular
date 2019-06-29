3c8d8ea4b444fc529117d61187c323ad460ec7a6
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-06-29 20:01:36 +0100

    [ Navbar ] » Estilos actualizados

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
