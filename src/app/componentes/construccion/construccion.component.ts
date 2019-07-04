12b1585b861d3ba3c61471e1c62dd2af4f99115e
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-07-04 07:59:04 +0100

    [ En construcción ] » Sección creada

---
+++ src/app/componentes/construccion/construccion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-construccion',
+  templateUrl: './construccion.component.html',
+  styles: []
+})
+export class ConstruccionComponent {
+
+  constructor() { }
+
+}
