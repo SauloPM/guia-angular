f87e6ece4a854a214d9ee4349cefa6cd471ac61f
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-07-04 10:19:52 +0100

    [ Rutas ] » Contenido actualizado

--- src/app/componentes/rutas/rutas.component.ts
+++ src/app/componentes/rutas/rutas.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-rutas',
   templateUrl: './rutas.component.html'
 })
-export class RutasComponent {
+export class RutasComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit() {
+
+    // Smooth scroll
+    $('.indice a').click( ( event ) => {
+
+      // Evitamos que cambie la URL
+      event.preventDefault();
+
+      // Almacenamos el destino
+      const target = event.currentTarget.hash;
+
+      // Nos desplazamos hasta la sección correspondiente
+      $('html, body').animate({
+        scrollTop: ( $(target).offset().top - 50 )
+      }, 500);
+
+    });
+
+  }
 
 }
