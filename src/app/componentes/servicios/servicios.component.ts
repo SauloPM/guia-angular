b2c338c0bde4f1a8cff65f7b82930af5cdb6d193
saulopm.1989@gmail.com <saulopm.1989@gmail.com>
2019-10-03 14:29:19 +0100

    [ Servicios ] » Sección creada

---
+++ src/app/componentes/servicios/servicios.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios',
+  templateUrl: './servicios.component.html'
+})
+export class ServiciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
