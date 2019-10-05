c49a8b0388e70a53e9aadd2890710bc4de74f106
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-06 00:44:41 +0100

    [ Componentes 'componentes' ] » jQuery eliminado

--- src/app/componentes/componentes/componentes.component.ts
+++ src/app/componentes/componentes/componentes.component.ts
@@ -1,35 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
-// jQuery
-declare var $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-componentes',
   templateUrl: './componentes.component.html',
   styles: []
 })
-export class ComponentesComponent implements OnInit {
+export class ComponentesComponent {
 
   constructor() { }
-
-  ngOnInit() {
-
-    // Smooth scroll
-    $('.indice a').click( ( event ) => {
-
-      // Evitamos que cambie la URL
-      event.preventDefault();
-
-      // Almacenamos el destino
-      const target = event.currentTarget.hash;
-
-      // Nos desplazamos hasta la sección correspondiente
-      $('html, body').animate({
-        scrollTop: ( $(target).offset().top - 50 )
-      }, 500);
-
-    });
-
-  }
 
 }
