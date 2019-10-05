27336d9d601852e1b48db91a8ad151bee8fdcb90
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-05 16:02:47 +0100

    [ Componente 'menu' ] » Estilos actualizados (responsive)

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -1,39 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
-declare var $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-
-  abrirDirectivas: boolean = false;
+export class MenuComponent {
 
   constructor() { }
-
-  ngOnInit() {
-
-    $('.item a').click( ( event ) => {
-
-      const id = '#' + event.target.id + ' .caret';
-
-      if ( !$(id).hasClass('active') ) {
-        $(id).addClass('active');
-      } else {
-        $(id).removeClass('active');
-      }
-
-    });
-
-  }
-
-  abrirSubmenu( item: string ) {
-    if ( item === 'directivas' ) {
-      this.abrirDirectivas = !this.abrirDirectivas;
-    } else {
-      this.abrirDirectivas = false;
-    }
-  }
+  
 }
