d26042c88e1315187573b506cf358ea1d01374f3
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-06 00:40:13 +0100

    [ Componente 'rutas' ] » Uso del componente 'indice'. jQuery eliminado

--- src/app/componentes/rutas/rutas.component.ts
+++ src/app/componentes/rutas/rutas.component.ts
@@ -1,33 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
-declare var $: any;
+// Interfaces
+import { ItemIndice } from 'src/app/interfaces/item-indice';
 
 @Component({
   selector: 'app-rutas',
   templateUrl: './rutas.component.html'
 })
-export class RutasComponent implements OnInit {
+export class RutasComponent {
+
+  itemsIndice: ItemIndice[] = [
+    {
+      texto: 'Fichero de rutas',
+      enlace: 'fichero-rutas'
+    },
+    {
+      texto: 'Módulo principal',
+      enlace: 'modulo-principal'
+    },
+    {
+      texto: 'Selector <b>&lt;router-outlet&gt;</b>',
+      enlace: 'router-outlet'
+    },
+    {
+      texto: 'Atributo <b>routerLink</b>',
+      enlace: 'routerlink'
+    },
+    {
+      texto: 'Atributo <b>routerLinkActive</b>',
+      enlace: 'routerlinkactive'
+    },
+    {
+      texto: 'Enviar parámetros dentro de la URL',
+      enlace: 'enviar-parametros'
+    },
+    {
+      texto: 'Recibir parámetros de la URL',
+      enlace: 'recibir-parametros'
+    },
+    {
+      texto: 'Rutas hijas',
+      enlace: 'rutas-hijas'
+    },
+    {
+      texto: 'Obtener parámetros del padre desde una ruta hija',
+      enlace: 'obtener-parametros-padre'
+    },
+    {
+      texto: 'Dividir el fichero de rutas en varios ficheros de rutas',
+      enlace: 'dividir-fichero-rutas'
+    },
+    {
+      texto: 'Manejo de rutas en Angular',
+      enlace: 'manejo-rutas'
+    }
+  ]
 
   constructor() { }
-
-  ngOnInit() {
-
-    // Smooth scroll
-    $('.indice a').click( ( event ) => {
-
-      // Evitamos que cambie la URL
-      event.preventDefault();
-
-      // Almacenamos el destino
-      const target = event.currentTarget.hash;
-
-      // Nos desplazamos hasta la sección correspondiente
-      $('html, body').animate({
-        scrollTop: ( $(target).offset().top - 50 )
-      }, 500);
-
-    });
-
-  }
 
 }
