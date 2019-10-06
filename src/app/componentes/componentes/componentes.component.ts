098c0a428de4cac23579d1a4975cf663f31ddd1a
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-06 01:54:00 +0100

    [ Componente 'componentes' ] » Uso del componente 'indice'

--- src/app/componentes/componentes/componentes.component.ts
+++ src/app/componentes/componentes/componentes.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
 
+// Interfaces
+import { ItemIndice } from 'src/app/interfaces/item-indice';
+
 @Component({
   selector: 'app-componentes',
   templateUrl: './componentes.component.html',
   styles: []
 })
 export class ComponentesComponent {
+
+  itemsIndice: ItemIndice[] = [
+    {
+      texto: 'Introducción',
+      enlace: 'introduccion'
+    },
+    {
+      texto: 'Crear componente',
+      enlace: 'crear-componente'
+    },
+    {
+      texto: 'Eliminar componente',
+      enlace: 'eliminar-componente'
+    },
+    {
+      texto: 'Editar componente',
+      enlace: 'editar-componente'
+    },
+    {
+      texto: 'Renderizar componente',
+      enlace: 'renderizar-componente'
+    },
+    {
+      texto: 'Enviar parámetros desde un componente padre a un componente hijo',
+      enlace: 'enviar-hijo'
+    },
+    {
+      texto: 'Enviar parámetros desde un componente hijo a un componente padre',
+      enlace: 'enviar-padre'
+    }
+  ]
 
   constructor() { }
 
