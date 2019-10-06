fa15b465c84a2722fe6fea2124fa9d50a5be8f2a
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-06 01:44:12 +0100

    [ Componente 'inicio' ] » Contenido actualizado

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+// Interfaces
+import { ItemIndice } from 'src/app/interfaces/item-indice';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html'
 })
 export class InicioComponent {
+
+  itemsIndice: ItemIndice[] = [
+    {
+      texto: 'Propósito',
+      enlace: 'proposito'
+    },
+    {
+      texto: 'Introducción',
+      enlace: 'introduccion'
+    },
+    {
+      texto: 'jQuery',
+      enlace: 'jquery'
+    }
+  ]
 
   constructor() { }
 
