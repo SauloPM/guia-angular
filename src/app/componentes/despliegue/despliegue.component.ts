9922e31e541eb4a24369064e6c3845b92b562fef
Saulo Poveda Montesdeoca <saulopm.1989@gmail.com>
2019-10-06 01:54:13 +0100

    [ Componente 'despliegue' ] » Uso del componente 'indice'

--- src/app/componentes/despliegue/despliegue.component.ts
+++ src/app/componentes/despliegue/despliegue.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interfaces
+import { ItemIndice } from 'src/app/interfaces/item-indice';
+
 @Component({
   selector: 'app-despliegue',
   templateUrl: './despliegue.component.html',
   styles: []
 })
 export class DespliegueComponent implements OnInit {
+
+  itemsIndice: ItemIndice[] = [
+    {
+      texto: 'Fichero environment.ts',
+      enlace: 'fichero-environment'
+    },
+    {
+      texto: 'Despliegue para preproducción',
+      enlace: 'despliegue-preproduccion'
+    },
+    {
+      texto: 'Despliegue para producción',
+      enlace: 'despliegue-produccion'
+    },
+    {
+      texto: 'Errores en despliegue de un servidor Apache',
+      enlace: 'errores-apache'
+    }
+  ]
 
   constructor() { }
 
